7563f6deaf3574b11a8e024b5fb0557d704e0baf
Omkar Bhagwat <obhagwat@cwoarchitects.ie>
2024-06-07 10:33:11 +0000

    Merged PR 25: keynote and validation

--- ResidentialApartments/Rooms/Bedroom.cs
+++ ResidentialApartments/Rooms/Bedroom.cs
@@ -4,8 +4,17 @@ using Autodesk.Revit.DB.Architecture;
 
 namespace RevitCore.ResidentialApartments.Rooms
 {
-    public class Bedroom(Room room) : RoomBase
+    public enum BedroomType
+    {
+        SingleBed,
+        DoubleBed
+    }
+    public class Bedroom(Room room ) : RoomBase
     {
         public override Room Room { get; } = room;
+
+        public BedroomType BedroomType { get; private set; }
+
+        public void SetBedRoomType(BedroomType type) => this.BedroomType = type;
     }
 }
